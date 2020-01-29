1288709530386314aab8aec15cf3503a87cd1794
sama08 <usamah_alan@hotmail.com>
2020-01-29 13:31:45 +0700

    First Commit
    Memamhami REST API

---
+++ Supermarket.API/Domain/Services/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using Supermarket.API.Domain.Models;
+using Supermarket.API.Domain.Services.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Supermarket.API.Domain.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> ListAsync();
+        Task<CategoryResponse> SaveAsync(Category category);
+        Task<CategoryResponse> UpdateAsync(int id, Category category);
+        Task<CategoryResponse> DeleteAsync(int id);
+    }
+}
---
+++ Supermarket.API/Startup.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Supermarket.API.Domain.Repositories;
+using Supermarket.API.Domain.Services;
+using Supermarket.API.Persistence.Contexts;
+using Supermarket.API.Persistence.Repositories;
+using Supermarket.API.Services;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Models;
+
+namespace Supermarket.API
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("supermarket-api-in-memory");
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Marketku", Version = "v1" });
+            });
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+
+            services.AddAutoMapper();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+              app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json","MyAPI");
+            });
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
